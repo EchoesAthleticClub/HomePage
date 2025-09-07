c1636bfa1f5c43904c7ade478ab5d64c07267fb3
DocteuMan <name.jellybeans511>
2025-09-07 16:24:31 +0900

    change commit

---
+++ src/data/members.js
@@ -0,0 +1,64 @@
+export const members = [
+    {
+      name: "Tsuyoshi🐢",
+      photo: "/tsuyoshi.jpg",
+      personalbest: {
+        "100m": "12.03",
+        "400m": "53.00",
+        "800m": "1:57.17",
+        "1500m": "4:05.07",
+        "5000m": "15:50.??",
+        "Marathon": "2:32:43",
+        "Long jump": "5m40cm"
+      },
+      intro: "弊チーム代表。シューズをいっぱぁE��ってぁE��。普段は研究老E��ランニングも探求らしい、E",
+    },
+    {
+      name: "Hiroki",
+      photo: "/blog-placeholder-4.jpg", // 画像パス
+      personalbest: {
+        "Marathon": "2:36",
+       },
+      intro: "陸上未経験、社会人から走り始めてマラソン2:36を達成したスーパ�Eランナ�E。目持E��は福岡国際�Eラソン出場�E�E��E",
+     },
+     {
+      name: "Takuto🐰",
+      photo: "/blog-placeholder-4.jpg", // 画像パス
+      personalbest: {
+        "100m": "?",
+       "Enbannage": "5m"
+      },
+      intro: "弊チームのスプリンター。理学療法士をもってぁE��エンジニア、E",
+     },
+    {
+      name: "Moeko🐹",
+      photo: "/moeko.png", // 画像パス
+      personalbest: {
+        "1500m":"4:45.65",
+        "3000m": "9:46.06",
+        "5000m": "16:35.99",
+        "Marathon": "3:21:25"
+      },
+      intro: "弊チームの紁E��点。中学時にはジュニアオリンピック、E��校時�E都道府県対抗駁E���E北海道代表、空知記録保持老E��輝かしい経歴。普段はエンジニア。本ホ�Eムペ�Eジも担当。重ぁE�Eを上げて高校ぶり�E自己記録更新で空知記録更新なるか�E�E��E",
+      },
+      {
+        name: "Sho🐶",
+        photo: "/blog-placeholder-2.jpg", // 画像パス
+        personalbest: {
+          "400m": "49.99",
+          "Marathon": "3:33"
+        },
+        intro: "弊チームのロングスプリンター、E00mアプローチで400mも走る。�Eラソンも走る、E",
+        },
+        
+         {
+            name: "Koya",
+            photo: "/blog-placeholder-4.jpg", // 画像パス
+            personalbest: {
+              "100m": "11.10"
+            },
+            intro: "我がチ�Eムのチ�Eター。陸上でしかド�Eパミンが�EなぁE��ンスター。世界陸上�E録画しなぁE��Day9まで現地観戦するのが彼の流儀、E",
+            },
+
+  ];
+
